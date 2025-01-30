9051974d855358d3148608c650d2cc51ea662d58
Verrels Eugeneo <vle2105@columbia.edu>
2025-01-30 13:53:46 -0800

    Added a template

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.getElementById("alertBtn");
+
+    if (btn) {
+        btn.addEventListener("click", function () {
+            alert("Hello! Welcome to my GitHub Pages site!");
+        });
+    }
+});
